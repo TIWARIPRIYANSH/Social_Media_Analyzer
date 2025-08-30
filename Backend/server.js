418b58eb6310fb8d05788fcd841ee85074dcf645
TIWARIPRIYANSH <2k22.cse.32884@gmail.com>
2025-08-30 18:14:38 +0530

    Updated cors for render

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,3 @@
-
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -8,26 +6,8 @@ const extractionRoutes = require('./routes/extractionRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://social-media-analyzer-45wb.onrender.com'
+app.use(cors());
 
-];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-      
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-          
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-};
-
-app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -36,7 +16,7 @@ app.use(express.json());
 app.use('/api', extractionRoutes);
 
 
-// --- Server Start ---
 app.listen(PORT, () => {
     console.log(`✅ Backend server is running and listening on http://localhost:${PORT}`);
 });
+
